66e26e8d1d8141fc95cbd6d450bb5d05b371a571
hibby <hibaqku7@gmail.com>
2025-04-19 12:02:56 +0300

    displayed questions using fetch method

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [ setQuestions] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then((r) => r.json)
+    .then((questions) => {
+      setQuestions(questions);
+    });
+  }, []);
   return (
     <section>
       <h1>Quiz Questions</h1>
